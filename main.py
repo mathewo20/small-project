753e7cccf4336d8fc962544c1fdb2a5c6dfa1375
mathewo20 <mo237704937@gmail.com>
2026-01-16 14:37:34 +0000

    figure out the loop

---
+++ main.py
@@ -0,0 +1,13 @@
+# this is a project that shows youre marking period average
+
+input("hello what is youre name: ")
+while True:
+    if isinstance(input, int):
+        print("wrong type you're real name" )
+    else:
+        print("hello" + str(input))
+        break
+
+# ask how many classes hes taking 
+
+input("how many classes are you taking")
